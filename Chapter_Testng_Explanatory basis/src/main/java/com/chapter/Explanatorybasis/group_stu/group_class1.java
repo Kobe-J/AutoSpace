5cfc509962e6ae61ff2c3d3ae4770962e31ec024
Kobe-yxl <2215557239@qq.com>
2020-04-09 18:25:45 +0800

    TestNG

---
+++ Chapter_Testng_Explanatory basis/src/main/java/com/chapter/Explanatorybasis/group_stu/group_class1.java
@@ -0,0 +1,11 @@
+package com.chapter.Explanatorybasis.group_stu;
+
+import org.testng.annotations.Test;
+
+@Test(groups = "kobe")
+public class group_class1 {
+    @Test
+    public void group_class4_case1(){
+        System.out.println("这里是group_class1");
+    }
+}
